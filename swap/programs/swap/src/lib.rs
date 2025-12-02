a006c4e604f3b9bc99e6d7abfe8e09a04bff8131
Adarsh Gupta <gadarsh318@gmail.com>
2025-12-02 18:54:44 +0545

    add project-7 swap program

--- swap/programs/swap/src/lib.rs
+++ swap/programs/swap/src/lib.rs
@@ -15,7 +15,18 @@ declare_id!("Fym2HxEWxZLtZPPgDYfGXprYiK712mxFx3Y13GQvyweX");
 pub mod swap {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        initialize::handler(ctx)
+    pub fn make_offer(
+        ctx: Context<MakeOffer>,
+        id: u64,
+        token_a_offered_amount: u64,
+        token_b_wanted_amount: u64,
+    ) -> Result<()> {
+        instructions::make_offer::send_offered_tokens_to_vault(&ctx, token_a_offered_amount)?;
+        instructions::make_offer::save_offer(ctx, id, token_b_wanted_amount)
+    }
+
+    pub fn take_offer(ctx: Context<TakeOffer>) -> Result<()> {
+        instructions::take_offer::send_wanted_tokens_to_maker(&ctx)?;
+        instructions::take_offer::withdraw_and_close_vault(ctx)
     }
 }
