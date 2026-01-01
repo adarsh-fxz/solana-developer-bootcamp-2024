9be1086b7d6b45d3c251f6255dd1571595ce4099
Adarsh Gupta <gadarsh318@gmail.com>
2026-01-01 18:34:35 +0545

    init

---
+++ token-lottery/anchor/programs/token_lottery/src/lib.rs
@@ -0,0 +1,12 @@
+#![allow(clippy::result_large_err)]
+
+use anchor_lang::prelude::*;
+
+declare_id!("Egh5nPhBQrrLRJ6ZyAQtnZ7LDj3Dtvj1hrxTBV5gQERe");
+
+#[program]
+pub mod token_lottery {
+    use super::*;
+
+    
+}
